c07d011ee4e446341a623d9c0b5340df927c83b4
Martin Bukatovic <mbukatov@redhat.com>
2016-11-28 19:28:25 +0100

    support for nested raut rage/model module trees

--- raut2webstr-pagemodel-tree.py
+++ raut2webstr-pagemodel-tree.py
@@ -25,49 +25,63 @@ import os
 import sys
 
 
-# list of python modules expected in page/model python module directory
-MODULES = ("models", "pages")
+# list of python modules expected in RAUT page/model python module directory
+RAUT_MODULES = ("models", "pages")
 
 
 def is_py_file(filename):
     return filename.endswith(".py") and filename != "__init__.py"
 
 
-def move_files(directory, src_module, src_files, dry_run=False, root_path=None):
+def move_files(directory, raut_module, src_directory, src_files,
+               dry_run=False, root_path=None):
     """
-    Move python files (provided in src_files) from given src_module
-    into new structure, which is created in given directory.
+    Move python files (provided in ``src_files`` list) from given
+    ``src_directory`` of a python module into new python module directory tree
+    structure.
+
+    Args:
+        directory: full file path of directory which contains raut python
+                   module directories (as listed in RAUT_MODULES list)
+        raut_module: name of raut module (item from RAUT_MODULES list)
+        src_directory (str): full file path of source (original) python module
+        src_files (list): list of python files (in src_directory)
+        dry_run (bool): don't touch anything, just show what "would be done"
+        root_path (str): full file path of a project
     """
+    # directory where new python modules created for each python source file from
+    # ``src_files`` are going to be created
+    new_directory = os.path.normpath(os.path.join(
+        directory,
+        os.path.relpath(
+            src_directory,
+            start=os.path.join(directory, raut_module))))
     for filename in src_files:
-        # dropping ".py" suffix
-        new_module_path = os.path.join(directory, filename[:-3])
-        new_module_ini_path = os.path.join(new_module_path, "__init__.py")
-        old_file_path = os.path.join(directory, src_module, filename)
-        new_file_path = os.path.join(new_module_path, "{}.py".format(src_module))
+        # we are going to create a *new python module* in ``new_directory`` for
+        # the python source file ``filename``
+        new_module_path = os.path.join(new_directory, filename[:-3])
+        new_module_init = os.path.join(new_module_path, "__init__.py")
+        old_file_path = os.path.join(src_directory, filename)
+        new_file_path = os.path.join(new_module_path, "{}.py".format(raut_module))
         if root_path is not None:
             import_path = os.path.relpath(new_module_path,
                                           root_path).replace('/', '.') + '.'
         if dry_run:
-            print('mkdir {}'.format(new_module_path))
-            print('touch {}'.format(new_module_ini_path))
+            print('mkdir -p {}'.format(new_module_path))
+            print('touch {}'.format(new_module_init))
+            print('mv {0} {1}'.format(old_file_path, new_file_path))
             if root_path is not None:
                 print('echo "import {0}{1}\n" >> {2}'.format(import_path,
-                                                             src_module,
-                                                             new_module_ini_path))
+                                                             module_basename,
+                                                             new_module_init))
         else:
-            try:
-                os.mkdir(new_module_path)
-            except FileExistsError:
-                pass
-            os.open(new_module_ini_path, os.O_CREAT, mode=0o664)
-            if root_path is not None:
-                ini_file = open(new_module_ini_path, 'a')
-                ini_file.write('import {0}{1}\n'.format(import_path, src_module))
-                ini_file.close()
-        if dry_run:
-            print('mv {0} {1}'.format(old_file_path, new_file_path))
-        else:
+            os.makedirs(new_module_path, exist_ok=True)
+            os.open(new_module_init, os.O_CREAT, mode=0o664)
             os.rename(old_file_path, new_file_path)
+            if root_path is not None:
+                ini_file = open(new_module_init, 'a')
+                ini_file.write('import {0}{1}\n'.format(import_path, module_basename))
+                ini_file.close()
 
 
 def main(argv=None):
@@ -89,38 +103,36 @@ def main(argv=None):
     if not os.path.isfile(os.path.join(args.directory, "__init__.py")):
         print("error: '{0}' is not a python module".format(args.directory))
         return 1
-    for subdir in [os.path.join(args.directory, i) for i in MODULES]:
+    for subdir in [os.path.join(args.directory, i) for i in RAUT_MODULES]:
         if not os.path.isdir(subdir):
             print("error: '{0}' directory is missing".format(subdir))
             return 1
 
-    # find python files
-    files = {} # page or model modules which we are going to rename
-    empty_init_files = [] # empty __init__.py files
-    for module in MODULES:
+    # do the transformation, happens in place
+    empty_init_files = [] # list of empty __init__.py files
+    for raut_module in RAUT_MODULES:
         for dirname, subdir_list, file_list in \
-                os.walk(os.path.join(args.directory, module)):
-            files[module] = [fl for fl in file_list if is_py_file(fl)]
+                os.walk(os.path.join(args.directory, raut_module)):
+            python_files = [fl for fl in file_list if is_py_file(fl)]
+            move_files(args.directory, raut_module, dirname, python_files,
+                       dry_run=args.dry_run, root_path=args.root_path)
             if "__init__.py" in file_list:
                 empty_init_files.append(os.path.join(dirname, "__init__.py"))
 
-    # do the transformation, happens in place
-    for mod in MODULES:
-        move_files(args.directory, mod, files[mod], dry_run=args.dry_run,
-                   root_path=args.root_path)
     # delete empty __ini__.py files in old page/model directories
     for init_file in empty_init_files:
         if args.dry_run:
             print("rm {}".format(init_file))
         else:
             os.remove(init_file)
-    # delete page/model directories (if empty)
-    for mod in MODULES:
-        mod_path = os.path.join(args.directory, mod)
-        if args.dry_run:
-            print("rmdir {}".format(mod_path))
-        elif len(os.listdir(mod_path)) == 0:
-            os.rmdir(mod_path)
+    # delete any remaining raut directories (if empty)
+    for raut_module in RAUT_MODULES:
+        for dirname, subdir_list, file_list in os.walk(
+                os.path.join(args.directory, raut_module), topdown=False):
+            if args.dry_run:
+                print("rmdir {}".format(dirname))
+            elif len(file_list) == 0:
+                os.rmdir(dirname)
 
 
 if __name__ == '__main__':
